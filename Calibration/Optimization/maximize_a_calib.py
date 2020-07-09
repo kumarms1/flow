9783f4a1acce0a343794165d910086de9c8d8a2e
Sadman Ahmed Shanto <sadman-ahmed.shanto@ttu.edu>
2020-07-09 16:54:32 -0500

    george version

--- Calibration/Optimization/maximize_a_calib.py
+++ Calibration/Optimization/maximize_a_calib.py
@@ -48,20 +48,27 @@ def adjustSize(sim, real):
 def setGuessedParams():
     return [float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])]
 
+#a,v0,T
 bounds = Bounds([-np.inf,11,1],[np.inf,30,3])
 guess = setGuessedParams()
-initial_error = getSpeedError(guess)
+best_error= getSpeedError(guess) #instead of guess used calibrated params
 
-print("Initial error: ", initial_error)
+print("Initial error: ", best_error)
 
 def getSpeedErrorPercentage(params):
     new_sim_error = getSpeedError(params)
-    percent_error = abs((initial_error - new_sim_error)/initial_error)
+    if best_error == 0:
+        percent_error = 0
+    else:
+        percent_error = abs((best_error - new_sim_error)/best_error) #divide by new sim eroro
     print("error percetange: ", percent_error)
     return percent_error
 
-error_constraint = NonlinearConstraint(getSpeedErrorPercentage,0,0.10,keep_feasible=True)
+error_constraint = NonlinearConstraint(getSpeedErrorPercentage,0,0.10)
 
+#calibration obj func?
+
+#sensitivity obj func
 def objective(params):
     a = params[0]
     print("value of a parameter: ", a)
